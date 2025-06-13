4d8a3fa07523025395faf451e52b8cf4f9338072
Марсель галимов <krutoichibik@gmail.com>
2025-06-13 19:57:15 +0500

    Update WebRequest.py

--- WebRequest.py
+++ WebRequest.py
@@ -4,7 +4,7 @@ def get(url):
     http = urllib3.PoolManager()
     site = http.request('GET', url)
     return site.data
-def get(url, text):
+def post(url, text):
     http = urllib3.PoolManager()
     site = http.request('POST', url, fields=text)
     return site.data
